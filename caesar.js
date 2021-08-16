b5f22d51055567566870ad4b63b194a962bc4c34
Saleh Jomar Salugandang <75149158+Saleh-Jomar@users.noreply.github.com>
2021-08-16 13:01:09 +0800

    Update caesar.js

--- caesar.js
+++ caesar.js
@@ -5,13 +5,13 @@ const caesar = function (string, shift) {
             const code = char.charCodeAt();
             if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
                 return String.fromCharCode(
-                    absMod(code + shift - codeset(code), 26) + codeset(code)
+                    Mod(code + shift - codeset(code), 26) + codeset(code)
                 );
             }
             return char;
         })
         .join("");
 };
-const absMod = (n, m) => (n % m + m) % m;
+const Mod = (n, m) => (n % m + m) % m;
 const codeset = (code) => code < 97 ? 65 : 97;
 module.exports = caesar
